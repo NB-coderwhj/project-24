d13ed8790f59174c85f3346ac75d5a85c987310d
NB-coderwhj <72655472+NB-coderwhj@users.noreply.github.com>
2021-06-08 17:20:36 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -13,7 +13,9 @@ function setup() {
 
 	paper1 = new Paper(200,320);
 	ground = new Ground(width/2,height-50,width,20);
-	dustbin = new Basket(600,330);
+	dustbin1 = new Dustbin(550,333,200,15);
+	dustbin2 = new Dustbin(459,276,15,100);
+	dustbin3 = new Dustbin(643,276,15,100);
 
 	Engine.run(engine);
   
@@ -25,12 +27,14 @@ function draw(){
 
 	paper1.display();
 	ground.display();
-	dustbin.display();
+	dustbin1.display();
+	dustbin2.display();
+	dustbin3.display();
 }
 
 function keyPressed(){
 	if(keyCode === 32){
-		Matter.Body.applyForce(paper1.body,paper1.body.position,{x:80, y:-50});
+		Matter.Body.applyForce(paper1.body,paper1.body.position,{x:60, y:-70});
 		//console.log("Key Pressed");
 	}
 }
